951b82caf78371f8ee1537fda9f96b4976779ca2
Frank Bardon Jr <bardonf@upenn.edu>
2014-08-06 10:46:28 -0400

    Updated kernel

--- src/Accard/Bundle/CoreBundle/Kernel/Kernel.php
+++ src/Accard/Bundle/CoreBundle/Kernel/Kernel.php
@@ -43,9 +43,7 @@ abstract class Kernel extends BaseKernel
             new \Accard\Bundle\CoreBundle\AccardCoreBundle(),
             new \Accard\Bundle\WebBundle\AccardWebBundle(),
             new \Accard\Bundle\ResourceBundle\AccardResourceBundle(),
-            //new \Accard\Bundle\FixturesBundle\AccardFixturesBundle(),
             new \Accard\Bundle\SettingsBundle\AccardSettingsBundle(),
-            //new \Accard\Bundle\ImportBundle\AccardImportBundle(),
 
             new \Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
             new \Symfony\Bundle\SecurityBundle\SecurityBundle(),
