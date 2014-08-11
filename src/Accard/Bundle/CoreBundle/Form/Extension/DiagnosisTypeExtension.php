fd00ff68cb1496584e2761b9cc0748b982a81985
Dylan Pierce <piercedy@upenn.edu>
2014-08-11 10:32:58 -0400

    Attempted to add phases to diagnosis form type extension.

---
+++ src/Accard/Bundle/CoreBundle/Form/Extension/DiagnosisTypeExtension.php
@@ -0,0 +1,75 @@
+<?php
+
+/**
+ * This file is part of the Accard package.
+ *
+ * (c) University of Pennsylvania
+ *
+ * For the full copyright and license information, please view the
+ * LICENSE file that was distributed with this source code.
+ */
+namespace Accard\Bundle\CoreBundle\Form\Extension;
+
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\AbstractTypeExtension;
+
+/**
+ * Diagnosis form type extension.
+ *
+ * @author Frank Bardon Jr. <bardonf@upenn.edu>
+ */
+class DiagnosisTypeExtension extends AbstractTypeExtension
+{
+    /**
+     * Patient class FQCN.
+     *
+     * @var string
+     */
+    protected $patientClass;
+
+    /**
+     * Phase class FQCN.
+     *
+     * @var string
+    */
+    // protected $phaseClass;
+
+
+    /**
+     * Constructor.
+     *
+     * @param string $patientClass
+     * @param string $phaseClass
+     */
+    public function __construct($patientClass /*, $phaseClass*/)
+    {
+        $this->patientClass = $patientClass;
+        // $this->phaseClass   = $phaseClass;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('patient', 'entity', array(
+                'class' => $this->patientClass,
+                'property' => 'fullName',
+            ))
+            // ->add('phase', 'entity', array(
+            //     'class' => $this->phaseClass,
+            //     'property' => 'label',
+            // ))
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getExtendedType()
+    {
+        return 'accard_diagnosis';
+    }
+}
