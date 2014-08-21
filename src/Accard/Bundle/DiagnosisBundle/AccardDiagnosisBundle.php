25a817d8923622ec2a0cf0344e5a49404599fb4e
Frank Bardon Jr <bardonf@upenn.edu>
2014-08-21 14:26:33 -0400

    Renamed diagnosis code groups from resource_* to diagnosis_*.

--- src/Accard/Bundle/DiagnosisBundle/AccardDiagnosisBundle.php
+++ src/Accard/Bundle/DiagnosisBundle/AccardDiagnosisBundle.php
@@ -41,8 +41,8 @@ class AccardDiagnosisBundle extends Bundle
     public function build(ContainerBuilder $container)
     {
         $interfaces = array(
-            'Accard\Component\Diagnosis\Model\CodeInterface' => 'accard.model.resource_code.class',
-            'Accard\Component\Diagnosis\Model\CodeGroupInterface' => 'accard.model.resource_code_group.class',
+            'Accard\Component\Diagnosis\Model\CodeInterface' => 'accard.model.diagnosis_code.class',
+            'Accard\Component\Diagnosis\Model\CodeGroupInterface' => 'accard.model.diagnosis_code_group.class',
             'Accard\Component\Diagnosis\Model\DiagnosisInterface' => 'accard.model.diagnosis.class',
             'Accard\Component\Diagnosis\Model\FieldInterface' => 'accard.model.diagnosis_field.class',
             'Accard\Component\Diagnosis\Model\FieldValueInterface' => 'accard.model.diagnosis_field_value.class',
