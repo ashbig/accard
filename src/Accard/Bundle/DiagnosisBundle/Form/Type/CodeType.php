567348c548b2c8f41a0210159f77cb53efa3599b
Frank Bardon Jr <bardonf@upenn.edu>
2014-08-21 14:39:15 -0400

    Created code and code group form types.

---
+++ src/Accard/Bundle/DiagnosisBundle/Form/Type/CodeType.php
@@ -0,0 +1,87 @@
+<?php
+
+/**
+ * This file is part of the Accard package.
+ *
+ * (c) University of Pennsylvania
+ *
+ * For the full copyright and license information, please view the
+ * LICENSE file that was distributed with this source code.
+ */
+namespace Accard\Bundle\DiagnosisBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+/**
+ * Diagnosis code form type.
+ *
+ * @author Frank Bardon Jr. <bardonf@upenn.edu>
+ */
+class CodeType extends AbstractType
+{
+    /**
+     * Data class.
+     *
+     * @var string
+     */
+    protected $dataClass;
+
+    /**
+     * Validation groups.
+     *
+     * @var array
+     */
+    protected $validationGroups;
+
+
+    /**
+     * Constructor.
+     *
+     * @param string $dataClass
+     * @param array $validationGroups
+     */
+    public function __construct($dataClass, array $validationGroups)
+    {
+        $this->dataClass = $dataClass;
+        $this->validationGroups = $validationGroups;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('code', 'text', array(
+                'label' => 'accard.diagnosis_code.form.code',
+            ))
+            ->add('description', 'text', array(
+                'label' => 'accard.diagnosis_code.form.description',
+            ))
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver
+            ->setDefaults(array(
+                'data_class' => $this->dataClass,
+                'validation_groups' => $this->validationGroups
+            ))
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'accard_diagnosis_code';
+    }
+}
