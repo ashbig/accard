cb779dc743fa6f45a1d358e38c034208da27745d
Frank Bardon Jr <bardonf@upenn.edu>
2015-08-05 08:48:26 -0400

    Removed unused bundles to reclaim some speed. Additionally, removed unused deprecation supression.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,30 +20,13 @@ class AppKernel extends Kernel
     /**
      * {@inheritdoc}
      */
-    public function __construct($environment, $debug)
-    {
-        parent::__construct($environment, $debug);
-
-        // We do not want deprecation warnings on the command line while
-        // we are transitioning... Remove me later.
-        if ($this->debug && php_sapi_name() == 'cli') {
-            //error_reporting(E_ALL & ~E_USER_DEPRECATED);
-        }
-    }
-
-    /**
-     * {@inheritdoc}
-     */
     public function registerBundles()
     {
         // Add extra bundles
         $bundles = array(
             new \Knp\Bundle\MenuBundle\KnpMenuBundle(),
             new \Symfony\Bundle\AsseticBundle\AsseticBundle(),
-            new \Accard\Bundle\PDSBundle\AccardPDSBundle(),
             new \DAG\Bundle\SecurityBundle\DAGSecurityBundle(),
-            new \Accard\Bundle\CPDBundle\AccardCPDBundle(),
-            new \Accard\Bundle\HMTBBundle\AccardHMTBBundle(),
             new \FOS\JsRoutingBundle\FOSJsRoutingBundle(),
             new \Accard\Bundle\UIBundle\AccardUIBundle(),
             new \Accard\Bundle\WebBundle\AccardWebBundle(),
